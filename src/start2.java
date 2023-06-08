f074eac663e0b46ef30ea54e635646df7d8409fd
Code-With-Nik <nikshaykataria3@gmail.com>
2023-06-08 19:50:01 +0530

    array prob 2

---
+++ src/start2.java
@@ -0,0 +1,11 @@
+public class start2 {
+    int sum(int a,int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        start2 obj=new start2();
+        int c= obj.sum(2,300);
+        System.out.println(c);
+    }
+}
