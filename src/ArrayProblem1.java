a630293247f992768d9e5e8e0f30f36e65afb9bb
Code-With-Nik <nikshaykataria3@gmail.com>
2023-06-08 19:45:03 +0530

    array prob 1

---
+++ src/ArrayProblem1.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+public class ArrayProblem1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        //que1
+      /*  float[] marks={45.7f,67.8f,99.2f,63.2f,100.0f};
+        float sum=0;
+         for(float i:marks){
+             sum+=i;
+             System.out.print(i+"  ");
+         }
+        System.out.println("\n");
+        System.out.println(sum);*/
+
+
+        //que2
+/*float[] marks={45.7f,52.2f,36.4f,25.2f};
+float num=45.74f;
+boolean isInarray=false;
+for(float i:marks){
+    if(num==i) {
+        isInarray = true;
+        break;
+    }
+
+    }if (isInarray){
+    System.out.println("VALUE IS PRESENT IN ARRAY");
+}else {
+            System.out.println("VALUE IS NOT PRESENT ");*/
+
+
+        //QUE 3
+    /*  int[][] mat1={{1,2,3},
+                           {4,5,6}};
+        int[][] mat2={{5,8,3},
+                {8,5,7}};
+        int[][] mat3=new int[2][3];
+        for(int i=0;i<mat3.length;i++){
+            for(int j=0;j<mat3[i].length;j++){
+                System.out.print(mat1[i][j]+mat2[i][j]+" ");
+            }
+            System.out.println();
+        }*/
+        //que4
+
+        /*int[] array={1,5,6,9,11,89};
+        int n=array.length;
+        for(int i=n-1;i>-1;i--){
+            System.out.print(array[i] +" ");
+        }*/
+
+
+
+        //reverse an array
+       /*int [] arr={1,2,5,3,6,4};
+       int l=arr.length;
+       int n=Math.floorDiv(l,2);
+       int temp;
+       for(int i=0;i<n;i++){
+           temp=arr[i];
+           arr[i]=arr[l-1-i];
+           arr[l-1-i]=temp;
+
+       }for(int element:arr){
+            System.out.println(element);
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
